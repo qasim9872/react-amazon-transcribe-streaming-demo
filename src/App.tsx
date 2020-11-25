97065ca4f304f0e2dd635be2287cb41fb2b1ca78
Muhammad Qasim <qasim9872@gmail.com>
2020-11-25 17:03:17 +0000

    added basic component placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/base/Navbar';
+import Footer from './components/base/Footer';
+import Home from './views/Home';
 
 function App() {
-  return <></>;
+  return (
+    <div className="relative">
+      <Router>
+        <Navbar />
+
+        <div>
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
