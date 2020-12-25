719bba93f2780ba23c06dc7f78ad83b7d3df6056
Muhammad Qasim <qasim9872@gmail.com>
2020-12-25 00:31:07 +0000

    dynammic layout selection refactor

--- src/sections/Main.tsx
+++ src/sections/Main.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import useSiteConfig from '../hooks/use-site-config';
-import Section from '../interfaces/Section';
-import SectionMap from './layouts/index';
-
-const wrapSection: React.FC<Section> = (details) => {
-  switch (details.type) {
-    case 'hero':
-      return SectionMap[details.type](details);
-    case 'about':
-      return SectionMap[details.type](details);
-    case 'blank':
-      return SectionMap[details.type](details);
-    default:
-      throw new Error('invalid type');
-  }
-};
+import buildLayout from './layouts/index';
 
 const MainSection: React.FC = () => {
   const siteConfig = useSiteConfig();
 
-  const sections = siteConfig.sections.map(wrapSection);
+  const sections = siteConfig.sections.map(buildLayout);
 
   return <div className="flex-grow">{sections}</div>;
 };
--- src/sections/layouts/index.ts
+++ src/sections/layouts/index.ts
@@ -1,11 +1,25 @@
 import Hero from './Hero';
 import About from './About';
 import Blank from './Blank';
+import Section from '../../interfaces/Section';
 
-const SectionMap = {
+const LayoutMap = {
   hero: Hero,
   about: About,
   blank: Blank,
 };
 
-export default SectionMap;
+const buildLayout: React.FC<Section> = (details) => {
+  switch (details.type) {
+    case 'hero':
+      return LayoutMap[details.type](details);
+    case 'about':
+      return LayoutMap[details.type](details);
+    case 'blank':
+      return LayoutMap[details.type](details);
+    default:
+      throw new Error('invalid type');
+  }
+};
+
+export default buildLayout;
