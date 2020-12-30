9aba79b8cb4340b284a4d2441a50649eeda816a5
Muhammad Qasim <qasim9872@gmail.com>
2020-12-30 04:38:44 +0000

    skill section finished

--- src/components/ui-components/SkillBarWrapper.tsx
+++ src/components/ui-components/SkillBarWrapper.tsx
@@ -3,15 +3,17 @@ import SkillBar from './SkillBar';
 
 const SkillBarWrapper: React.FC<{
   componentName: 'SkillBarWrapper';
-  bottomLineDivConfig: ComponentProps<typeof SkillBar>[];
-}> = ({ bottomLineDivConfig }) => {
+  skills: ComponentProps<typeof SkillBar>[];
+}> = ({ skills }) => {
   return (
     <div className="py-10 flex flex-col lg:flex-row lg:flex-wrap justify-between">
-      {bottomLineDivConfig.map((config) => (
-        <SkillBar key={config.name} {...config}>
-          {' '}
-        </SkillBar>
-      ))}
+      {skills
+        .sort((skill1, skill2) => skill2.percentage - skill1.percentage)
+        .map((config) => (
+          <SkillBar key={config.name} {...config}>
+            {' '}
+          </SkillBar>
+        ))}
     </div>
   );
 };
