8a60e83c235e103d3aa32a6e7ce669c6934260a6
Muhammad Qasim <qasim9872@gmail.com>
2020-12-31 03:03:23 +0000

    animations for nav header

--- src/components/app/base/SideNav/NavHeader.tsx
+++ src/components/app/base/SideNav/NavHeader.tsx
@@ -6,7 +6,7 @@ export const NavImage: React.FC<{ src: string }> = ({ src }) => {
     <img
       src={src}
       alt="Author profile"
-      className="h-36 w-36 object-cover rounded-full"
+      className="h-36 w-36 object-cover rounded-full transition-all duration-500 ease-in-out transform hover:rotate-90 hover:scale-110"
     />
   );
 };
@@ -17,11 +17,16 @@ export const NavHeader: React.FC = () => {
   return (
     <div className="flex flex-col items-center py-3">
       <NavImage src={author.avatar} />
-      <h1 className="pt-5 text-xl font-bold">{author.name}</h1>
-      <span className="uppercase pt-2 text-sm">
-        <span className="text-blue-400">{author.jobTitle}</span> in{' '}
-        {author.location}
-      </span>
+      <h1 className="pt-5 text-xl font-bold transition-all duration-500 ease-in-out transform hover:scale-110">
+        {author.name}
+      </h1>
+      <div className="group inline-flex uppercase pt-2 text-sm">
+        <h2 className="text-blue-400 transition-all duration-500 ease-in-out transform group-hover:scale-110">
+          {author.jobTitle}
+        </h2>
+        &nbsp;
+        <h2>in {author.location}</h2>
+      </div>
     </div>
   );
 };
