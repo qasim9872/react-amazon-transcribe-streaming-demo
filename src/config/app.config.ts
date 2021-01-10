b3808bb1b41676880ed627c7a0e35215ec81f91d
Muhammad Qasim <qasim9872@gmail.com>
2021-01-10 21:09:22 +0000

    updated process.env access

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,4 @@
-const { env } = process;
-
-export const { NODE_ENV, REACT_APP_NAME, REACT_APP_LOG_LEVEL } = env;
+export const { NODE_ENV, REACT_APP_NAME, REACT_APP_LOG_LEVEL } = process.env;
 export const IS_PROD = NODE_ENV === 'production';
 
 export const NAME = REACT_APP_NAME || "Qasim's portfolio";
