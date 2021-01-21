399bcd009cf3473ac02e9a0ea8623241f35ee485
Muhammad Qasim <qasim9872@gmail.com>
2021-01-21 15:59:27 +0000

    wip - updated name

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,5 @@
 export const { NODE_ENV, REACT_APP_NAME, REACT_APP_LOG_LEVEL } = process.env;
 export const IS_PROD = NODE_ENV === 'production';
 
-export const NAME = REACT_APP_NAME || 'React Chatbot';
+export const NAME = REACT_APP_NAME || 'React Amazon Transcribe';
 export const LOG_LEVEL = REACT_APP_LOG_LEVEL || 'info';
