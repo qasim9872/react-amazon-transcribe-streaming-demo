7d80ab77e473211eeed8886131c13e1903cd0c66
Muhammad Qasim <qasim9872@gmail.com>
2021-01-22 19:18:10 +0000

    refactor

--- src/components/ui-components/DynamicTranscribeForm/index.tsx
+++ src/components/ui-components/DynamicTranscribeForm/index.tsx
@@ -20,7 +20,7 @@ const DynamicTranscribeForm: React.FC<{
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.persist();
     if (typeof setTranscribeConfig === 'function') {
-      console.log({ [event.target.name]: event.target.value });
+      //   console.log({ [event.target.name]: event.target.value });
       setTranscribeConfig({
         ...transcribeConfig,
         [event.target.name]: event.target.value,
--- src/components/ui-components/Hello.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Hello: React.FC<{
-  componentName: 'Hello';
-}> = () => {
-  return <div className="flex-grow flex py-8 rounded-full">hello world</div>;
-};
-
-export default Hello;
